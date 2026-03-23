0d769583b094501b37f8e85011ee7ad0cec55081
Matheus <178436024+maxmati1@users.noreply.github.com.>
2026-03-22 22:35:08 -0300

    Adiciona repository Tarefa

---
+++ src/main/java/com/bn/ex10/repositories/TarefaRepository.java
@@ -0,0 +1,7 @@
+package com.bn.ex10.repositories;
+
+import com.bn.ex10.models.TarefaModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TarefaRepository extends JpaRepository<TarefaModel, Long> {
+}
